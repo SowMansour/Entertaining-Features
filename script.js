eab2cf72db113c85d79d80e76b007e684012b35e
SowMansour <mansoursow10@gmail.com>
2023-08-09 17:52:27 +0200

    youtubeFeature done :white_check_mark:

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Connecté !');
-
 /**
  * Pseudo-code:
  * Au clic (événement) sur le bouton accepter(evenement)
@@ -22,3 +20,30 @@ const bannerFeature = () => {
 };
 
 bannerFeature();
+
+/**
+ * La func youtubeFeature alterne les états de l'emoji
+ * et du bouton subscribe au clic
+ */
+const youtubeFeature = () => {
+  const emoji = document.querySelector('i');
+
+  emoji.addEventListener('click', () => {
+    emoji.classList.toggle('fa-smile-wink');
+    emoji.classList.toggle('btn-happy');
+  });
+
+  const btnbutton = document.querySelector('.btn');
+
+  btnbutton.addEventListener('click', () => {
+    btnbutton.classList.toggle('subscriber');
+ 
+    if(btnbutton.innerText === 'Subscribe'){
+      btnbutton.innerText = `You're subscribed`;
+    } else {
+      btnbutton.innerText = 'Subscribe';
+    }
+  });
+};
+
+youtubeFeature();

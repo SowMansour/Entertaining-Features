3239a719c97c70ea342e20212905e11173c033df
SowMansour <mansoursow10@gmail.com>
2023-08-11 01:32:35 +0200

    modaleFeature done :white_check_mark:

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const bannerFeature = () => {
   const btnSucces =  document.querySelector('.accept-cookies');
 
   // Je Sélectionne et stock la Section cookie
-  const cookies = document.querySelector('.footer');
+  const cookies = document.querySelector('.cookie');
 
   // Je SOUMETS la section cookie à une action au clic
   btnSucces.addEventListener('click', function(){
@@ -47,3 +47,33 @@ const youtubeFeature = () => {
 };
 
 youtubeFeature();
+
+// Step 1: Je selectionne et stock les éléments nécessaire
+const modal= document.querySelector('.modal-container');
+
+const btn = document.querySelector('.btn-success');
+
+const code = document.querySelector('.code');
+
+const icone = document.querySelector('.fa-times');
+
+//Step 3: Je les soumets aux évements
+
+window.addEventListener('scroll', displayModal);
+function displayModal(){
+  const hauteur = document.documentElement.scrollTop;
+  if(hauteur > 640){
+    modal.style.display = 'flex'; /**display passe de none à flex(cf:modal-container) */
+  }
+}
+
+btn.addEventListener('click', () => {
+  code.style.display = 'block'; //visible
+  btn.style.display = 'none'; //invisible
+});
+
+icone.addEventListener('click', () => {
+  modal.style.display = 'none';
+  window.removeEventListener('scroll', displayModal);
+});
+
